82c6b4fb21f3fc34291a20c6eb42b71ab466aa5e
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2023-11-17 14:58:16 -0500

    creando extructura inicial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import RolesComponent from "./components/RoleManagement";
+
 function App() {
+  //arreglo de roles iniciales
   const initialRoles = [
-    { id: '1', name: 'Admin', permissions: ['PROJECT:WRITE', 'STORE:READ', 'ACCOUNT:READ_ACCESS'] },
-    { id: '2', name: 'Editor', permissions: ['PROJECT:READ', 'STORE:WRITE'] },
-  ]
-  
-  const initialPermissions = ['PROJECT:READ', 'PROJECT:WRITE', 'STORE:READ', 'STORE:WRITE', 'ACCOUNT:READ_ACCESS'];
+    {
+      id: "1",
+      name: "Admin",
+      permissions: ["PROJECT:WRITE", "STORE:READ", "ACCOUNT:READ_ACCESS"],
+    },
+    { id: "2", name: "Editor", permissions: ["PROJECT:READ", "STORE:WRITE"] },
+  ];
+
+  //arreglo de permisos inicales
+  const initialPermissions = [
+    "PROJECT:READ",
+    "PROJECT:WRITE",
+    "STORE:READ",
+    "STORE:WRITE",
+    "ACCOUNT:READ_ACCESS",
+  ];
 
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <RolesComponent
+        initialRoles={initialRoles}
+        initialPermissions={initialPermissions}
+      />
     </div>
   );
 }
---
+++ src/components/RoleManagement.jsx
@@ -0,0 +1,11 @@
+const RolesComponent = ({initialRoles, initialPermissions}) => {
+    console.log("🚀 - RolesComponent - initialPermissions:", initialPermissions)
+    console.log("🚀 - RolesComponent - initialRoles:", initialRoles)
+    return (  
+        <div>
+            <h3>desde roles components</h3>
+        </div>
+    );
+}
+ 
+export default RolesComponent;
