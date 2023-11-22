70621ca64459b672b83fefa69b3b15d43d1dd192
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2023-11-22 11:49:24 -0500

    Refactorizando EncabezadoPermiso

--- src/components/components/EncabezadoPermiso.jsx
+++ src/components/components/EncabezadoPermiso.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { capitalizarPermiso } from "../../helpers/capitalizarUtils";
 import papeleraIcon from "../../papelera.png";
 
-const EncabezadoPermizo = ({
+const EncabezadoPermiso = ({
   p,
   roles,
   entidad,
@@ -13,55 +13,34 @@ const EncabezadoPermizo = ({
   const [mostrarIconosPermiso, setMostrarIconosPermiso] = useState(false);
   const [permisoCheked, setPermisoCheked] = useState(false);
 
-  const handleMouseEnterPermiso = () => {
-    setMostrarIconosPermiso(true);
-  };
-  const handleMouseLeavePermiso = () => {
-    setMostrarIconosPermiso(false);
-  };
+  const handleMouseEnterPermiso = () => setMostrarIconosPermiso(true);
+  const handleMouseLeavePermiso = () => setMostrarIconosPermiso(false);
 
-  const handleCheckboxChangePermiso = (permiso, entidad) => {
-    const nombreFull = `${entidad}:${permiso}`;
-
-    if (permisoCheked) {
-      const updateRoles = roles.map((rol) => {
-        const updatePermissions = rol.permissions.filter(
-          (permiso) => permiso !== nombreFull
-        );
-        return { ...rol, permissions: updatePermissions };
-      });
-      setRoles(updateRoles);
-    } else {
-      const updateRoles = roles.map((rol) => {
-        // Verificar si el permiso ya existe en el arreglo de permissions
-        if (!rol.permissions.includes(nombreFull)) {
-          const nuevosPermisos = [...rol.permissions, nombreFull];
-          return { ...rol, permissions: nuevosPermisos };
-        } else {
-          // Si el permiso ya existe, devolver el rol sin cambios
-          return rol;
-        }
-      });
-      setRoles(updateRoles);
-    }
-
+  const handleCheckboxChangePermiso = () => {
+    const nombreFull = `${entidad}:${p}`;
+    const updateRoles = roles.map((rol) => {
+      const updatePermissions = permisoCheked
+        ? rol.permissions.filter((permiso) => permiso !== nombreFull)
+        : [...rol.permissions, nombreFull];
+      return { ...rol, permissions: updatePermissions };
+    });
+    setRoles(updateRoles);
     setPermisoCheked(!permisoCheked);
   };
 
-  const borrarPermiso = (permiso, entidad) => {
-    const nombreFull = `${entidad}:${permiso}`;
-    const updatedPermissions = permisos.filter(
-      (permiso) => permiso !== nombreFull
-    );
+  const borrarPermiso = () => {
+    const nombreFull = `${entidad}:${p}`;
+    const updatedPermissions = permisos.filter((permiso) => permiso !== nombreFull);
     setPermisos(updatedPermissions);
   };
 
   const permisoCapitalizado = capitalizarPermiso(p);
+
   return (
     <th
-    className="encabezado-Permiso"
-      onMouseEnter={() => handleMouseEnterPermiso()}
-      onMouseLeave={() => handleMouseLeavePermiso()}
+      className="encabezado-Permiso"
+      onMouseEnter={handleMouseEnterPermiso}
+      onMouseLeave={handleMouseLeavePermiso}
     >
       {mostrarIconosPermiso ? (
         <div className="celdas">
@@ -70,7 +49,7 @@ const EncabezadoPermizo = ({
               type="checkbox"
               name="ceckbocxPermiso"
               defaultChecked={permisoCheked}
-              onChange={() => handleCheckboxChangePermiso(p, entidad)}
+              onChange={handleCheckboxChangePermiso}
             />
             {permisoCapitalizado}
           </div>
@@ -79,7 +58,7 @@ const EncabezadoPermizo = ({
               className="icono-papelera"
               src={papeleraIcon}
               alt="Eliminar"
-              onClick={() => borrarPermiso(p, entidad)}
+              onClick={borrarPermiso}
             />
           </div>
         </div>
@@ -90,4 +69,4 @@ const EncabezadoPermizo = ({
   );
 };
 
-export default EncabezadoPermizo;
+export default EncabezadoPermiso;
